c3a5c01dc351aa76a6774a3abe6dfbfa54960900
zefluxo <luisosorio2001@gmail.com>
2024-05-11 20:27:11 +0100

    feat: added logout

---
+++ project/app/src/routes/logout/+page.server.ts
@@ -0,0 +1,19 @@
+import type { PageServerLoad } from "./$types";
+import { fail, redirect } from "@sveltejs/kit";
+
+export const load: PageServerLoad = async ({ cookies }) => {
+
+    const userStr = cookies.get("user");
+
+    if (!userStr) {
+        redirect(303, "/");
+    }
+
+    cookies.delete("user", { 'path': '/' });
+        
+    // TODO: make the page linger for a bit
+    redirect(302, '/');
+
+
+};
+
---
+++ project/app/src/routes/logout/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>Logout</title>
+</svelte:head>
+
+<div class="w-1/2 mx-auto text-center flex items-center justify-center">
+    <h2 class="w-1/4">
+        Logging out...
+    </h2>
+</div>
