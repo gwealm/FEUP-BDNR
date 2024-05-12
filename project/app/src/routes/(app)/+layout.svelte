ff14b438cb748b9e4d539f95182ef11cf31d290a
Guilherme Almeida <gui1612@sapo.pt>
2024-05-12 17:44:17 +0100

    feat: adds dropdown for @me and logout

--- project/app/src/routes/(app)/+layout.svelte
+++ project/app/src/routes/(app)/+layout.svelte
@@ -8,24 +8,33 @@
     const { servers, user } = data;
 </script>
 
-<section
-    class="flex w-16 flex-col items-center gap-2 overflow-x-hidden overflow-y-scroll py-2 scrollbar-hide"
-    id="server-list"
->
+<section class="scrollbar-hide flex w-16 flex-col items-center gap-2 overflow-visible py-2" id="server-list">
     <!-- TODO: This should be factored out into a component that also has a list of DMs (if those are added) -->
-    <div class="h-12 w-12">
-        <a href="/@me">
-            <img
-                src="/favicon.png"
-                alt="Image for {'@me'}"
-                class="rounded-xl transition-all duration-100 ease-in-out hover:bg-blue-300"
-            />
-        </a>
+    <div class="dropdown dropdown-right">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="h-12 w-12 rounded-xl">
+                <img
+                    src="/favicon.png"
+                    alt="Image for {'@me'}"
+                    class="rounded-xl transition-all duration-100 ease-in-out hover:bg-blue-300"
+                />
+            </div>
+        </div>
+        <ul
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box ml-3 w-52 p-2 shadow"
+        >
+            <li>
+                <a class="justify-between" href="/@me">
+                    Profile
+                </a>
+            </li>
+            <li><a href="/logout">Logout</a></li>
+        </ul>
     </div>
     <div class="divider mx-1 mb-0 mt-0 h-1 rounded bg-white" />
     <ServerList {servers} />
 
-    <div class="mt-auto mb-2">
+    <div class="mb-2 mt-auto">
         <a href="/logout">
             <Icon name="log-out" height="2em" width="2em" />
         </a>
