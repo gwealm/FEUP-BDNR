81a06260175c3f7ab423e45e3c5905c199694c90
Guilherme Almeida <gui1612@sapo.pt>
2024-05-12 16:24:16 +0100

    feat: implements user search

--- project/app/src/lib/components/server/member/list.svelte
+++ project/app/src/lib/components/server/member/list.svelte
@@ -1,19 +1,29 @@
 <script lang="ts">
-    import type { Server } from "$lib/types";
+    import type { Server, User } from "$lib/types";
     import Member from "./item.svelte";
 
     export let server: Server;
+    let searchQuery = '';
 
     $: members = Object.values(server.members);
+
+    // @ts-ignore im blue dabadi dabada dabadi dabada dabadi dabada dabadi dabada
+    $: filteredMembers = members.filter((member: User) => 
+        member.username.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 </script>
 
 <aside class="flex w-80 flex-col items-center gap-5 bg-zinc-700 py-4">
-    <header class="text-2xl">
+    <header class="text-2xl text-white">
         <h2>Members</h2>
     </header>
-    <section class="mt-2">
+    <div class="form-control w-70 px-2">
+        <input type="text" placeholder="Search members..." class="input input-bordered w-full"
+            bind:value={searchQuery} />
+    </div>
+    <section class="flex-grow overflow-auto mt-2">
         <ul class="flex flex-col gap-5">
-            {#each members as member (`member-${member.id}`)}
+            {#each filteredMembers as member (`member-${member.id}`)}
                 <li>
                     <Member {member} />
                 </li>
