c69b8c63f0da563b25885f4efa06bf2f994b44b0
zefluxo <luisosorio2001@gmail.com>
2024-05-13 16:11:04 +0100

    chore: get rid of useless register button

--- project/app/src/routes/login/+page.svelte
+++ project/app/src/routes/login/+page.svelte
@@ -119,30 +119,6 @@
                     </span>
                     Sign in
                 </button>
-                <a href="/register">
-                    <button
-                        type="submit"
-                        class="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 mt-4"
-                        >
-                        <span
-                            class="absolute inset-y-0 left-0 flex items-center pl-3"
-                        >
-                            <svg 
-                                class="h-5 w-5 text-blue-500 group-hover:text-blue-400"
-                                fill="currentColor" 
-                                xmlns="http://www.w3.org/2000/svg" 
-                                viewBox="0 0 310 310"
-                                aria-hidden="true"
-                            >
-                                <path d="M300.606,159.727l-45.333-45.333c-5.857-5.858-15.355-5.858-21.213,0L225,123.454V15c0-8.284-6.716-15-15-15H20
-                                   C11.716,0,5,6.716,5,15v240.002c0,8.284,6.716,15,15,15h85V295c0,8.284,6.716,15,15,15h45.333c3.979,0,7.794-1.581,10.607-4.394
-                                   l124.667-124.667C306.465,175.081,306.465,165.585,300.606,159.727z M35,30h160v123.454l-85.606,85.605
-                                   c-0.302,0.301-0.581,0.619-0.854,0.942H35V30z M159.12,280H135v-24.121l109.667-109.666l24.12,24.12L159.12,280z"/>
-                            </svg>
-                        </span>
-                        Register
-                    </button>
-                </a>
                 {#if form?.errors?.general}
                 <p class="mt-2 text-center text-red-500">
                     {form?.errors?.general}
