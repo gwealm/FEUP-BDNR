7d8ac6d0a7e2e713164908301c45f6cea86d20ef
Nuno Pereira <nunoafonso2002@gmail.com>
2024-05-14 02:10:42 +0000

    Added initial queries script

--- project/db/scripts/populate.sh
+++ project/db/scripts/populate.sh
@@ -5,8 +5,8 @@ sleep 2
 
 docker run \
     -it \
-    --mount type=bind,source=./scripts/populate_scripts,target=/populate_scripts \
+    --mount type=bind,source=./scripts/db_scripts,target=/db_scripts \
     aerospike/aerospike-tools:latest \
     aql \
     -h "$(docker inspect -f '{{.NetworkSettings.IPAddress }}' "$1")" \
-    -f "/populate_scripts/$2"
+    -f "/db_scripts/$2"
